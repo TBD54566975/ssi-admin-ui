f6829fbab2d4c15062201a7206cf1f34f2529909
Michael Neale <michael.neale@gmail.com>
2023-03-08 13:33:11 +1100

    checking it loads

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Page load test', () => {
+  it('should load the page with an HTML tag', () => {
     cy.visit('http://localhost:3000')
+    cy.get('html').should('exist')
   })
 })
