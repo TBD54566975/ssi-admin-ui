282336abbec0dc64e9d87805211acde7d170cd1c
Kirah Sapong <kirah@tbd.email>
2023-03-24 16:17:42 -0700

    Clean up select

--- src/composables/Select.tsx
+++ src/composables/Select.tsx
@@ -1,19 +1,47 @@
-import { Component, For, JSX, JSXElement } from 'solid-js';
+import { Component, For, JSX } from 'solid-js';
 import Icon from '../icons/Icon';
 import './_select.css';
 
-const Select: Component<{handleEvent?: JSX.EventHandlerUnion<HTMLSelectElement, InputEvent>, label: string, description?: string, name: string, options: Array<{label: string, value: string, disabled?: boolean, selected?: boolean}>, firstIsDefault?: boolean, disabled?: boolean, optional?: boolean}>  = (props) => {
+interface SelectInterface {
+    handleEvent?: JSX.EventHandlerUnion<HTMLSelectElement, InputEvent>, 
+    label: string, 
+    description?: string, 
+    name: string, 
+    options: Array<{
+        label: string, 
+        value: string, 
+        disabled?: boolean, 
+        selected?: boolean
+    }>, 
+    firstIsDefault?: boolean, 
+    disabled?: boolean, 
+    optional?: boolean
+}
+
+const Select: Component<SelectInterface>  = (props) => {
     return (
         <div class="field-container">
             <label for={props.name} class="field-heading">{props.label}</label>
             {props.optional && <label class="secondary-label">(Optional)</label>}
             {props.description && <label for={props.name} class="field-description">{props.description}</label>}
             <div class="select-area">
-                <select required={!props.optional} oninput={props.handleEvent} name={props.name} id={props.name} disabled={props.disabled}>
+                <select 
+                    required={!props.optional} 
+                    oninput={props.handleEvent} 
+                    name={props.name} 
+                    id={props.name} 
+                    disabled={props.disabled}
+                >
                     <option disabled selected>Select one</option>
                     <For each={props.options}>
                         {(option, index) =>
-                            <option value={option.value} disabled={option.disabled} selected={option.selected}>{option.label}{index() === 0 && props.firstIsDefault ? ' (Default)': ''}</option> 
+                            <option 
+                                value={option.value} 
+                                disabled={option.disabled} 
+                                selected={option.selected}
+                            >
+                                {option.label}{index() === 0 && props.firstIsDefault ? ' (Default)': ''}
+                            </option> 
                         }
                     </For>
                 </select>
